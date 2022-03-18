99081cb3348321e5b826a34c392d296ab1926410
EUGEarg <eugeniarivarossa@gmail.com>
2022-03-18 19:31:37 -0300

    arreglos header

---
+++ header.js
@@ -0,0 +1,9 @@
+
+ const btnResponsive = document.querySelector('.btn-responsive');
+ const barraNav = document.querySelector('.navbar');
+ const barras = document.querySelectorAll('.btn-responsive span')
+ 
+ btnResponsive.addEventListener('click', () => {
+     barraNav.classList.toggle('activado');
+     barras.forEach(child => { child.classList.toggle('animado')});
+ })
