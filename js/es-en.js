cea015cf1c8e4206776e68887ed0b5ba1b3cceed
EUGEarg <eugeniarivarossa@gmail.com>
2022-03-23 11:39:49 -0300

    ajustes idioma

---
+++ js/es-en.js
@@ -0,0 +1,12 @@
+let check = document.querySelector('.check');
+check.addEventListener('click', idioma);
+
+function idioma() {
+    let id = check.checked;
+
+    if(id == true) {
+        location.href = 'es/index.html';
+    } else {
+        location.href = '../index.html';
+    }
+}
